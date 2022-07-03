8d3fe0a03a9554ae6429e88559cfeec371306990
E-Lexis <elexisblash@gmail.com>
2022-07-03 08:26:32 -0400

    Started on typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBook: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
